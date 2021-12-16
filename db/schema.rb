9a98bdf27bf372440ee83d4484a9a9faa5efd78e
coralisme <vhnowflearning@Gmail.com>
2021-12-16 11:34:05 +0000

    Update style

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_12_16_052242) do
+ActiveRecord::Schema.define(version: 2021_12_16_061257) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
