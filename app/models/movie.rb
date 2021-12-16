aa0052d95416790b3070d25d71fd34c282e7cc1b
vhnowf <vohoangnamntm@Gmail.com>
2021-12-16 03:01:27 +0000

    Add style for review form

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ApplicationRecord
     has_one_attached :image
     validates :title, :actors, :directors, :category, length:{ minimum: 1}
-    validates :description, length:{ minimum: 20}
+    validates :description, length:{ minimum: 10}
     validates :length, numericality: { only_integer: true }
     has_many :movie_review, -> { order "created_at DESC"}
 end
