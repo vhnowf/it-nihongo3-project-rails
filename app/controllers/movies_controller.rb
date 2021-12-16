e0eabc604033be38a282d7984adf6602048e35d4
vhnowf <vohoangnamntm@Gmail.com>
2021-12-16 02:49:56 +0000

    add authenciation

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   before_action :set_movie, only: %i[ show edit update destroy ]
-
+  before_action :authenticate_user!
   # GET /movies or /movies.json
   def index
     @movies = Movie.all
